30f9e04f094f60bacac3be40b9cf946ecbd94155
mythr <mythr@LAPTOP-78LDJ37C>
2024-10-21 19:39:16 -0700

    Add project files.

---
+++ MVCDBFirstExample/Controllers/EmployeePracticeController.cs
@@ -0,0 +1,24 @@
+﻿using MVCDBFirstExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDBFirstExample.Controllers
+{
+    public class EmployeePracticeController : Controller
+    {
+        SampleEntities1 db = new SampleEntities1();
+        // GET: EmployeePractice
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
